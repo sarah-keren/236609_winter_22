66b0dee8d1a880a6934fa7ffc0df4cdf440f5107
masarwy-m <90391513+masarwy-m@users.noreply.github.com>
2023-02-05 20:01:06 +0200

    Update costmap_listen_and_update.py

--- scripts/costmap_listen_and_update.py
+++ scripts/costmap_listen_and_update.py
@@ -10,27 +10,32 @@ from map_msgs.msg import OccupancyGridUpdate
 
 class CostmapUpdater:
     def __init__(self):
+        self.number_of_update = 0
         self.cost_map = None
         self.shape = None
+        self.map_org = None
+        self.resolution = None
         rospy.Subscriber('/move_base/global_costmap/costmap', OccupancyGrid, self.init_costmap_callback)
         rospy.Subscriber('/move_base/global_costmap/costmap_updates', OccupancyGridUpdate, self.costmap_callback_update)
 
     def init_costmap_callback(self, msg):
-        print('only once')  # For the student to understand
         self.shape = msg.info.height, msg.info.width
         self.cost_map = np.array(msg.data).reshape(self.shape)
+        self.map_org = np.array([msg.info.origin.position.x, msg.info.origin.position.y])
+        self.resolution = msg.info.resolution
 
     def costmap_callback_update(self, msg):
-        print('periodically')  # For the student to understand
         shape = msg.height, msg.width
         data = np.array(msg.data).reshape(shape)
         self.cost_map[msg.y:msg.y + shape[0], msg.x: msg.x + shape[1]] = data
-        self.show_map()  # For the student to see that it works
 
     def show_map(self):
         if not self.cost_map is None:
             plt.imshow(self.cost_map)
             plt.show()
+
+    def position_to_map(self, pos):
+        return (pos - self.map_org) // self.resolution
 
 
 if __name__ == '__main__':
